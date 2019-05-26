7ee570a57a378722bd76fb22d7a1498d9f92c1dc
jwjung <jjwonyop@gmail.com>
2019-05-26 23:56:35 +0900

    repository, mapper setting

---
+++ src/main/java/com/newbie/bulletinboard/domain/repositories/posts/PostMapper.java
@@ -0,0 +1,7 @@
+package com.newbie.bulletinboard.domain.repositories.posts;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PostMapper {
+}
--- src/main/java/com/newbie/bulletinboard/domain/services/members/MemberServiceImpl.java
+++ src/main/java/com/newbie/bulletinboard/domain/services/members/MemberServiceImpl.java
@@ -1,4 +1,13 @@
 package com.newbie.bulletinboard.domain.services.members;
 
+import com.newbie.bulletinboard.domain.repositories.members.MemberMapper;
+import com.newbie.bulletinboard.domain.repositories.members.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberServiceImpl {
+    private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
 }
