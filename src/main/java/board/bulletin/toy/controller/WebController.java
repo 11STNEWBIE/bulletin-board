e5d8d4b3b8900c405e3efdb05555585c596fcb48
dongmin <ifmoon.io@gmail.com>
2019-06-16 21:13:50 +0900

    View 구현

---
+++ src/main/java/board/bulletin/toy/controller/WebController.java
@@ -0,0 +1,20 @@
+package board.bulletin.toy.controller;
+
+import board.bulletin.toy.service.PostsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+
+    private PostsService postsService;
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.getPosts());
+        return "main";
+    }
+}
